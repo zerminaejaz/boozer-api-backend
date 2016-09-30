3a8efab940c3415eb294dc2ecb924141ef4cc387
ipc103 <iancandysemail@gmail.com>
2016-09-30 16:18:13 -0400

    update solution

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -10,7 +10,12 @@ module Api
       end
 
       def create
+        cocktail = Cocktail.create(name: cocktail_params[:name])
 
+        cocktail_params[:proportions].each do |proportion|
+           ingredient = Ingredient.find_or_create_by(name: proportion[:ingredient_name])
+           proportion = Proportion.create(amount: proportion[:amount], ingredient: ingredient, cocktail: cocktail)
+        end
       end
 
       def edit
@@ -23,6 +28,12 @@ module Api
 
       def destroy
 
+      end
+
+      private
+
+      def cocktail_params
+        params.require(:cocktail).permit(:name, :description, :instructions,  proportions: [:ingredient_name, :amount])
       end
     end
   end
